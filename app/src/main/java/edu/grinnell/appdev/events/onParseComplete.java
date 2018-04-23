1d51f96170228376502e99eef55b159831a5f29e
lamsalab <am.lamsal@gmail.com>
2018-04-22 21:11:54 -0500

    Forgot to add onParseComplete interface

---
+++ app/src/main/java/edu/grinnell/appdev/events/onParseComplete.java
@@ -0,0 +1,13 @@
+package edu.grinnell.appdev.events;
+
+
+import java.util.List;
+
+/**
+ * Provides methods to implement that is used by the Parser class
+ * to notify the UI thread once the XML string has been parsed.
+ */
+public interface onParseComplete {
+    void onParseComplete(List<Event> eventList);
+    void onParseFail(String failMsg);
+}
