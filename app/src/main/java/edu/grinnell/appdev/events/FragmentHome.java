ed2710ce208019fc3bcca353d1f389c315c9802a
lamsalab <am.lamsal@gmail.com>
2018-08-23 10:35:43 -0500

    Recycler view state is restored

--- app/src/main/java/edu/grinnell/appdev/events/FragmentHome.java
+++ app/src/main/java/edu/grinnell/appdev/events/FragmentHome.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
@@ -29,7 +30,8 @@ public class FragmentHome extends Fragment {
     public HomeRecyclerViewAdapter recyclerViewAdapter;
     public ArrayList<Event> eventArrayList;
     private Activity activity;
-
+    private RecyclerView.LayoutManager layoutManager;
+    private Parcelable parcelable;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -48,7 +50,7 @@ public class FragmentHome extends Fragment {
     public void configureRecyclerView(Activity activity){
         RecyclerView recyclerView = getView().findViewById(R.id.my_recycler_view);
         recyclerView.hasFixedSize();
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(activity);
+        layoutManager = new LinearLayoutManager(activity);
         recyclerView.setLayoutManager(layoutManager);
         recyclerViewAdapter = new HomeRecyclerViewAdapter(activity, eventArrayList);
         recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), VERTICAL));
@@ -68,6 +70,10 @@ public class FragmentHome extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
         this.activity = getActivity();
+
+        // Retrieve list state and list/item positions
+        if(savedInstanceState != null)
+            parcelable = savedInstanceState.getParcelable("Events list");
     }
 
     /**
@@ -109,6 +115,24 @@ public class FragmentHome extends Fragment {
         setUpSearch(menu);
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle state) {
+        super.onSaveInstanceState(state);
+
+        // Save list state
+        parcelable = layoutManager.onSaveInstanceState();
+        state.putParcelable("Events list", parcelable);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        //Restore the original state of recycler view
+        if (parcelable != null) {
+            layoutManager.onRestoreInstanceState(parcelable);
+        }
+    }
 
 
 }
